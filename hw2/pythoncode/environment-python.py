6e533c36a5d59aa049ecba4a627d5a65f871d188
nadineapresto <rapresto@ucsd.edu>
2026-02-02 22:38:04 -0800

    add env python

---
+++ hw2/pythoncode/environment-python.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+import os
+
+print("Cache-Control: no-cache")
+print("Content-Type: application/json")
+print()
+
+print("<!DOCTYPE html>")
+print("<html><head><title>Environment Variables</title>")
+print("</head><body><h1 align=\"center\">Environment Variables</h1>")
+print("<hr>")
+
+# Loop through all environment variables and print names and values
+for key, value in sorted(os.environ.items()):
+    print(f"<b>{key}:</b> {value}<br />")
+
+print("</body></html>")
