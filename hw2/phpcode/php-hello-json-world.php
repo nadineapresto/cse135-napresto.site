cd140c74917f08297585c84dc85e7d3dc731c130
nadineapresto <rapresto@ucsd.edu>
2026-02-02 16:47:54 -0800

    add php hello json world

---
+++ hw2/phpcode/php-hello-json-world.php
@@ -0,0 +1,14 @@
+<?php
+// Set content type to JSON
+header("Content-Type: application/json");
+
+// Get data from user to display on webpage
+$datetime = date("Y-m-d H:i:s");
+$ip_address = $_SERVER['REMOTE_ADDR'];
+
+// Build JSON message with user data
+$message = ('title' => 'Hello, PHP!', 'heading' => 'Hello, PHP! From Nadine Apresto!', 'message' => 'This page was generated with the PHP programming language', 'time' => $datetime, 'IP' => $ip_address)
+
+// Convert to JSON message and output for webpage
+echo json_encode($message);
+?>
