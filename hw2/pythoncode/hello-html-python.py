7ae3e37faf68a994cb78f3c957166727bea2a473
nadineapresto <rapresto@ucsd.edu>
2026-02-02 22:16:19 -0800

    fixing php file naming + adding python hello html

---
+++ hw2/pythoncode/hello-html-python.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+import datetime
+import os
+
+print("Cache-Control: no-cache")
+print("Content-Type: text/html")
+
+# Get user data to print
+date_time = datetime.datetime.now().strftime("%a %b %e %H:%M:%S %Y")
+ip_address = os.environ.get('REMOTE_ADDR', 'Unknown')
+
+print("<!DOCTYPE html>")
+print("<html>")
+print("<head>")
+print("<title>Hello, Python!</title>")
+print("</head>")
+print("<body>")
+print("<h1 align=center>Hello Python World</h1><hr/>")
+print("<p>Hello World from Nadine Apresto!</p>")
+print("<p>This page was generated with the Python programming langauge</p>")
+print(f"<p>This program was generated at: {date_time}</p>")
+print(f"<p>Your current IP Address is: {ip_address}</p>")
+print("</body>")
+print("</html>")
